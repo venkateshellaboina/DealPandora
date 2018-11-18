1f0130382f554bee48c8ef7052ca8b16c20d3d7d
venkateshellaboina@gmail.com <https://www.github.com/venkateshellaboina>
2018-11-18 11:31:59 +0530

    server

--- server.js
+++ server.js
@@ -25,17 +25,48 @@ app.use(function (req, res, next) {
 );
 
 var ProductSchema = new Schema({
-    productId: {type:number},
+    productId: {type:Number},
     productName: {type:String},
     productCategory: {type:String},
     productSpecification : {type:String},
-    productPrice : {type:number},
+    productPrice : {type:Number},
     productImageURL : {type:String},
     productSearchTextURL : {type:String}
 
 })
 
+var userSchema = new Schema({
+    userName : {type: String},
+    mobileNo : {type : String}
+})
+
+var CardSchema = new Schema({
+    userName : {type : String},
+    cardTag  :{type:String},
+    cardNo: {type:String},
+    nameOnCard : {type:String},
+    month :{type: Number},
+    year : {type: Number},
+    bankId : {type: Number}
+})
+
+var BankSchema = new Schema ({
+    bankId : {type: Number},
+    bankName : {type : String}
+})
+
+var OfferSchema = new Schema ({
+    productId: {type: Number},
+    bankId : {type : Number},
+    productDiscountPrice : {type: String}
+})
+
+
+
 var ProductModel = mongoose.model("product",ProductSchema);
+var BankModel = mongoose.model("bank",BankSchema);
+var OfferModel = mongoose.model("offer",OfferSchema);
+var CardModel = mongoose.model("card",CardSchema);
 
 app.get("http://localhost:6666/getProducts",(req,res)=>{
     ProductModel.findOne({productId:"123"})
@@ -50,3 +81,32 @@ app.get("http://localhost:6666/getProducts",(req,res)=>{
         }
     })
 })
+
+app.get("http://localhost:6666/getOffers/:productId",(req,res)=>{
+    OfferModel.find({productId: req.params.productId})
+    .exec((err,result)=>{
+        if(err){
+            res.send(err);
+            res.end();
+        }
+        else{
+            res.send(result);
+            res.end();
+        }
+
+    })
+})
+
+app.post("http://localhost:6666/addCard",(req,res)=>{
+    CardModel.create({
+        userName : req.body.name,
+        cardNo: req.body.cardNo,
+        nameOnCard : req.body.nameOnCard,
+        month : req.body.cardNo,
+        year : req.body.year,
+        bankId : req.body.bankId
+    })
+
+})
+
+
